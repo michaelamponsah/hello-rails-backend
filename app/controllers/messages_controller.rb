b1f21fa25cb08b6ca97199b86b774146700dd2a4
michaelamponsah <amponsahmichaelkwame@gmail.com>
2023-05-26 19:18:04 +0000

    Fix linter errs

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,8 @@
 class MessagesController < ApplicationController
   def random_greeting
-    begin
-      @greeting = Message.order("RANDOM()").first
-      render json: { greeting: @greeting.greeting }
-    rescue StandardError => e
-      render json: {error: e.message, status: :unprocessable_entity}
-    end
+    @greeting = Message.order('RANDOM()').first
+    render json: { greeting: @greeting.greeting }
+  rescue StandardError => e
+    render json: { error: e.message, status: :unprocessable_entity }
   end
 end
